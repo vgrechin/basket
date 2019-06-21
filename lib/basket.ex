05efdc84ab151f63bc848c87e7c2de53cacb3f6a
Viacheslav Grechin <vgrechin@luxoft.com>
2019-06-21 14:36:33 +0300

    First query to KDB

--- lib/basket.ex
+++ lib/basket.ex
@@ -18,11 +18,20 @@ defmodule Basket do
 
           with { kdb, _ } <- Kdb.open( '127.0.0.1', 5001, 'testusername', 'testpassword', :infinity ) do
                IO.inspect( kdb, label: "Success" )
+               query( kdb, "3.23 6.46" )
                Kdb.close( kdb )
           else
                { :error, reason } -> IO.inspect( reason, label: "Error" )
           end
 
           { :ok, self() }
+     end
+
+     defp query( socket, arg ) do
+          with { :ok, { type, result } } <- Kdb.sync( socket, { :char_list, to_charlist( arg ) } ) do
+               IO.inspect( { type, result }, label: "Result" )
+          else
+               { :error, reason } -> IO.inspect( reason, label: "Error" )
+          end
      end
 end
