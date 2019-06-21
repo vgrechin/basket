d49039665d0fbd5e44c572acc3066ef509d91b46
Viacheslav Grechin <vgrechin@luxoft.com>
2019-06-21 13:35:32 +0300

    Open KDB connection

--- lib/basket.ex
+++ lib/basket.ex
@@ -3,6 +3,8 @@ NimbleCSV.define( CSVParser, separator: "," )
 defmodule Basket do
      @constituents "deps/SnP500s/data/constituents.csv"
 
+     alias :qErlang, as: Kdb
+
      def start( _type, _args ) do
           { :ok, contents } = File.read( @constituents )
 
@@ -13,6 +15,13 @@ defmodule Basket do
                |> Map.new
 
           IO.inspect( constituents, label: "Constituents", limit: :infinity )
+
+          case Kdb.open( '127.0.0.1', 5001, '', '', 1000 ) do
+               { :error, reason } -> IO.inspect( reason, label: "Error" )
+               { kdb, _ } ->
+                    IO.inspect( kdb, label: "Success" )
+                    Kdb.close( kdb )
+          end
 
           { :ok, self() }
      end
