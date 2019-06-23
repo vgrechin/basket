d4e833e1ca45a574e8a118091236fd53250ef983
Slava Grechin <slava.grechin@gmail.com>
2019-06-23 03:13:29 +0300

    Price added

--- lib/Market/constituent.ex
+++ lib/Market/constituent.ex
@@ -1,12 +1,12 @@
 defmodule Market.Constituent do
      use GenServer
-     alias :os, as: Os
      alias :rand, as: Rand
 
      def init( { ticker, constituent } ) do
+          price = 10 + Rand.uniform( 990 )
           timeout = Rand.uniform( 1000 )
           timer = Process.send_after( self(), :tick, timeout )
-          state = %{ ticker: ticker, constituent: constituent, timer: timer }
+          state = %{ ticker: ticker, price: price, constituent: constituent, timer: timer }
 
           { :ok, state }
      end
@@ -14,8 +14,9 @@ defmodule Market.Constituent do
      def handle_info( :tick, state ) do
           timeout = Rand.uniform( 1000 )
           timer = Process.send_after( self(), :tick, timeout )
-          IO.inspect( state.ticker, label: "Ticker")
+          price = state.price + ( Rand.uniform - 0.5 ) * state.price / 100
+          IO.inspect( { state.ticker, price }, label: "Equity")
 
-          { :noreply, %{ state | timer: timer } }
+          { :noreply, %{ state | price: price, timer: timer } }
      end
 end
