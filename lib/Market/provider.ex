0dd1bc09c0be3b8f1385570f17c6375c6ff4b539
Viacheslav Grechin <vgrechin@luxoft.com>
2019-07-12 16:48:21 +0300

    Distributed network tests

--- lib/Market/provider.ex
+++ lib/Market/provider.ex
@@ -35,8 +35,11 @@ defmodule Market.Provider do
      end
 
      def handle_cast( { :query, date, time, symbol, price, volume }, state ) do
-          with { :ok, { type, result } } <- Kdb.sync( state.connection, { :char_list, to_charlist(
-                    "`basket insert (#{date};#{time}0;`#{symbol};#{price + state.skew * price};#{volume})" ) } ) do
+          query = "`basket insert (#{date};#{time}0;`#{symbol};#{price + state.skew * price};#{volume})"
+          #IO.inspect( query, label: "Query")
+          #query = "insertTrade [#{date};#{time}0;`#{symbol};#{price + state.skew * price};#{volume}]"
+
+          with { :ok, { type, result } } <- Kdb.sync( state.connection, { :char_list, to_charlist( query ) } ) do
                { :noreply, state }
           else
                { :error, reason } -> IO.inspect( reason, label: "Error" )
--- lib/basket.ex
+++ lib/basket.ex
@@ -17,7 +17,7 @@ defmodule Basket do
                     end )
                |> Map.new
 
-          with { connection, _ } <- Kdb.open( '127.0.0.1', 5001, 'testusername', 'testpassword' ) do
+          with { connection, _ } <- Kdb.open( '172.31.13.203', 5001, 'testusername', 'testpassword' ) do
                IO.inspect( connection, label: "Success" )
                Basket.Supervisor.start_link( connection )
                trade( basket )
@@ -35,6 +35,6 @@ defmodule Basket do
 
      defp trade( basket ) do
           constituents = Enum.reduce basket, %{}, &spawn/2
-          Process.sleep( :infinity )
+          Process.sleep( 60000 )
      end
 end
